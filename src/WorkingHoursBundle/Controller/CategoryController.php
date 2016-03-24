590c066a4222d3da07e44303a8dd987c3816f7ca
Ieva Jaksaityte <ieva.jaksaityte@gmail.com>
2016-03-25 01:34:58 +0200

    categories controller and view can now add new categories

--- src/WorkingHoursBundle/Controller/CategoryController.php
+++ src/WorkingHoursBundle/Controller/CategoryController.php
@@ -2,18 +2,52 @@
 
 namespace WorkingHoursBundle\Controller;
 
+use WorkingHoursBundle\Entity\categories;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CategoryController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('WorkingHoursBundle:Category:index.html.twig');
+        $categories = $this->getDoctrine()->getRepository('WorkingHoursBundle:categories')->findAll();
+        
+        return $this->render('WorkingHoursBundle:Category:index.html.twig', array('categories' => $categories));
     }
     
-    public function createAction()
+    public function createAction(Request $request)
     {
-        return $this->render('WorkingHoursBundle:Category:create.html.twig');
+        $category = new categories;
+        
+        $form = $this->createFormBuilder($category)
+                ->add('title', TextType::class, array('label' => 'Antraštė', 'attr' => array('class' => 'form-control')))
+                ->add('description', TextareaType::class, array('label' => 'Aprašymas', 'attr' => array('class' => 'form-control')))
+                ->add('save', SubmitType::class, array('label' => 'Įrašyti', 'attr' => array('class' => 'btn btn-primary')))
+                ->getForm();
+        
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {
+            $title = $form['title']->getData();
+            $description = $form['description']->getData();
+            
+            $category->setTitle($title);
+            $category->setDescription($description);
+            
+            $em = $this->getDoctrine()->getManager();
+            
+            $em->persist($category);
+            $em->flush();
+            
+            $this->addFlash('notice', 'Kategorija pridėta');
+            
+            return $this->redirectToRoute('working_hours_category_index');
+        }
+        
+        return $this->render('WorkingHoursBundle:Category:create.html.twig', array('form' => $form->createView()));
     }
     
     public function editAction($id)
